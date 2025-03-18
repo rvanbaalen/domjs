12699fd3665f15f3af23af34e72c4c01b0db51e6
Robin van Baalen <robin@ganttify.com>
2025-03-17 22:52:37 -0400

    fix: update base URL in Vite configuration for proper deployment

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { resolve } from 'path'
 
 export default defineConfig({
-  //base: "/custom-base-url/", // Change this if your page is not hosted at a root URL
+  base: "/domjs/", // Change this if your page is not hosted at a root URL
   plugins: [
     tailwindcss(),
   ],
